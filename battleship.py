e5f914d622c61d47ed87374bd7d54326826ad431
Nic Bonetto <nick.bonetto@gmail.com>
2017-09-18 14:16:51 -0700

    Created Battleship board. #1

---
+++ battleship.py
@@ -0,0 +1,12 @@
+from random import randint
+
+board = []
+
+for item in range(5):
+    board.append(['O'] * 5)
+
+def print_board(board_in):
+    for row in board_in:
+        print ' '.join(row)
+
+print_board(board)
